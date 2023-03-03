053721d0d0feef836f77c10283b56dd3721fe47e
Semih Helvaci <semih.helvaci92@gmail.com>
2023-03-04 00:36:14 +0100

    Add ale config

---
+++ dot_config/nvim/after/plugin/ale.lua
@@ -0,0 +1,10 @@
+vim.g.ale_fixers = {
+	css = { "prettier" },
+	javascript = { "prettier" },
+	typescript = { "prettier" },
+	lua = { "stylua" },
+	sh = { "shfmt" },
+}
+vim.g.ale_fix_on_save = 1
+vim.g.ale_linters_explicit = 1
+vim.g.ale_javascript_prettier_options = "--trailing-comma all"
