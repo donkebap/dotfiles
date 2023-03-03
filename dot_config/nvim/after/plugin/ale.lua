37eb7ca7d96a5759d40cb189b025728484341651
Semih Helvaci <semih.helvaci92@gmail.com>
2023-03-04 00:52:24 +0100

    Fix prettier trailing-comma option

--- dot_config/nvim/after/plugin/ale.lua
+++ dot_config/nvim/after/plugin/ale.lua
@@ -7,4 +7,4 @@ vim.g.ale_fixers = {
 }
 vim.g.ale_fix_on_save = 1
 vim.g.ale_linters_explicit = 1
-vim.g.ale_javascript_prettier_options = "--trailing-comma all"
+vim.g.ale_javascript_prettier_options = "--trailing-comma es5"
